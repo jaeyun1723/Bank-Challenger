8469d93b25c31c101f3d13b46348a961374bcf1e
Mukho <mukho@kakao.com>
2023-05-09 22:58:55 +0900

    Rename BankChall

--- src/main/java/com/boolsazo/bankchall/BankChallApplication.java
+++ src/main/java/com/boolsazo/bankchall/BankChallApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankchallApplication {
+public class BankChallApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankchallApplication.class, args);
+        SpringApplication.run(BankChallApplication.class, args);
     }
 
 }
--- src/test/java/com/boolsazo/bankchall/BankChallApplicationTests.java
+++ src/test/java/com/boolsazo/bankchall/BankChallApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BankchallApplicationTests {
+class BankChallApplicationTests {
 
     @Test
     void contextLoads() {

d9e3dee3fdead147c36f42379c0473dc2ecb6a1d
Mukho <mukho@kakao.com>
2023-05-09 19:23:54 +0900

    Backend init

---
+++ src/main/java/com/boolsazo/bankchall/BankchallApplication.java
@@ -0,0 +1,13 @@
+package com.boolsazo.bankchall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankchallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankchallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boolsazo/bankchall/BankchallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boolsazo.bankchall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankchallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

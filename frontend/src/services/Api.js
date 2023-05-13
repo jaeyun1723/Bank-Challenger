390a16e994349605bbc83c34ab0fd43c081a8a6d
Mukho <mukho@kakao.com>
2023-05-14 08:28:48 +0900

    DOCS: Update README.md
    - Update how to run
    - Fix typo

---
+++ frontend/src/services/Api.js
@@ -0,0 +1,8 @@
+const Api = {
+    client_id : '253Merwmxma9AJ7gNb0O',
+    client_secret : 'TN1LZFMW7n',
+    callback_url : 'http://localhost:3000/login/naver',
+    state : '1107'
+};
+
+export default Api;

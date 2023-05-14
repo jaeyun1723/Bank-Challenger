9ac4fdb47dfc1103b0a720c9b2f3178c610b1f5d
Mukho <mukho@kakao.com>
2023-05-14 13:52:18 +0900

    Feat: Update BankChallApplication
    - Prevent run spring security

--- src/main/java/com/boolsazo/bankchall/BankChallApplication.java
+++ src/main/java/com/boolsazo/bankchall/BankChallApplication.java
@@ -2,8 +2,9 @@ package com.boolsazo.bankchall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BankChallApplication {
 
     public static void main(String[] args) {

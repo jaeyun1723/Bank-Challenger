8071428a8a6877876ac3211b78087a7bdce4ceba
ashine-k <84389492+ashine-k@users.noreply.github.com>
2023-05-25 10:45:46 +0900

    Feat. 메인페이지 추가, 디자인 이미지 추가 (#75)
    * Feat : "쥐 이미지 추가"
    
    * Feat : "쥐 이미지 추가" Fix : "필요없는 라이브러리 삭제"
    
    * Add: Add image
    
    * Feat : "메인페이지 디자인 추가", "쥐 이미지 추가"
    
    * Delete 파일 삭제
    
    * FEAT. 쥐 이미지, 메인 디자인 추가, main.js페이지 추가

---
+++ frontend/src/pages/IndexSections/Main04.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Main04 extends React.Component {
+  render() {
+    return (
+      <>
+        <section
+          className="section section-components pb-0"
+          id="section-components"
+          style={{
+            backgroundImage: `url(${require("../../assets/img/theme/main04.png")})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh",
+          }}
+        >
+          {}
+        </section>
+      </>
+    );
+  }
+}
+
+export default Main04;
---
+++ frontend/src/pages/IndexSections/Main05.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Main05 extends React.Component {
+  render() {
+    return (
+      <>
+        <section
+          className="section section-components pb-0"
+          id="section-components"
+          style={{
+            backgroundImage: `url(${require("../../assets/img/theme/main05.png")})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh",
+          }}
+        >
+          {}
+        </section>
+      </>
+    );
+  }
+}
+
+export default Main05;

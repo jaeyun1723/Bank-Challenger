88f26f2a3541bdbb19f9e5001d77d6b565175ef8
HyeYoung Shin <nanda5050@naver.com>
2023-05-25 18:16:44 +0900

    fix: 제목 올리기 (#86)
    * FEAT: 금융대사량 페이지 추가
    
    * Add: 금융대사량 Api 연결
    
    * Merge branch 'develop' of https://github.com/boolsazo/bank-challenger into feature/bfrTest
    
    # Conflicts:
    #	frontend/src/pages/BfrTest.js
    
    * fix: 제목 올리기

--- frontend/src/pages/BfrResult.js
+++ frontend/src/pages/BfrResult.js
@@ -22,33 +22,30 @@ class Login extends React.Component {
           <main ref="main">
             <section className="section section-shaped section-lg">
               <div className="shape shape-style-1 shape-default">
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
+
               </div>
               <Container className="pt-lg-7">
-                <Card container spacing={2}>
+                <h2 className="display-3 text-white" style={{textAlign: 'center'}}>
+                  {name}님과 같은 {bfr}에 대해 알려드릴게요!
+                </h2>
+
+                <Card container spacing={2} style={{backgroundColor: "#768ff4"}}>
                   <Grid container>
                     <Grid item xs style={{textAlign: "center"}}>
                       <img
                           alt="..."
                           width={"60%"}
                           style={{margin: "30px"}}
-                          src={require("assets/img/mouses/HIF.png")}
+                          src={require("assets/img/statistics/HIF.png")}
                       />
                     </Grid>
                     <Divider orientation="vertical" flexItem/>
                     <Grid item xs>
-                      <h2 className="display-3 mb-0"
+                      <h2 className="display-3 mb-0 text-white"
                           style={{textAlign: 'center'}}>
                         {bfr}
                       </h2>
-                      <span>
+                      <span className="description d-none d-md-inline-block text-white">
                       투자를 할 때 위험한 상품을 선호하지만, 때로는 충동적으로 투자를 하기도 합니다. 예를 들어, 주식이나 부동산과 같은 상품에 투자를 하기도 합니다.
                       미래를 위해 저축하고 투자하는 것을 좋아합니다. 예를 들어, 노후를 위해 저축을 하거나, 자녀의 교육비를 위해 저축을 합니다.
                     </span>
@@ -57,10 +54,6 @@ class Login extends React.Component {
                 </Card>
               </Container>
               <Container className="pt-lg-7">
-                <h2 className="display-3 text-white"
-                    style={{textAlign: 'center'}}>
-                  {name}님과 같은 {bfr}에 대해 알려드릴게요!
-                </h2>
                 <Statistics/>
               </Container>
             </section>

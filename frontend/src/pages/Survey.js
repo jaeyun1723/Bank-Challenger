dd113060e850595458a1757f06028e5225c6051a
HyeYoung Shin <nanda5050@naver.com>
2023-05-29 23:34:01 +0900

    Style: 설문조사 폼 수정 (#148)

--- frontend/src/pages/Survey.js
+++ frontend/src/pages/Survey.js
@@ -1,20 +1,30 @@
-import { React, useState } from "react";
+import {React, useState} from "react";
 import axios from "axios";
 import {
-  Card,
   Box,
-  FormGroup,
-  FormLabel,
-  RadioGroup,
-  Radio,
-  MenuItem,
-  Input,
-  Select,
-  Slider,
   Button,
   FormControlLabel,
-  Typography,
+  FormGroup,
+  FormLabel,
+  Grid,
+  Input,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Slider,
+  Typography
 } from "@mui/material";
+import {experimentalStyled as styled} from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({theme}) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function Survey() {
   const userId = sessionStorage.getItem("userId");
@@ -69,11 +79,11 @@ function Survey() {
 
   const handleConfirmInput = () => {
     const message = `결혼 여부 : ${isMarried}\n월 소득 : ${numberWithCommas(
-      Math.round(monthlyIncome)
+        Math.round(monthlyIncome)
     )}만 원\n소비 비율 : ${spendingRatio}%\n직종 : ${occupation}\n예적금 : ${numberWithCommas(
-      Math.round(savings)
+        Math.round(savings)
     )}만 원\n대출금 : ${numberWithCommas(
-      Math.round(loan)
+        Math.round(loan)
     )}만 원\n위 정보가 맞습니까?`;
 
     if (window.confirm(message)) {
@@ -91,20 +101,24 @@ function Survey() {
         method: "POST",
         url: "/survey",
         data: sendData,
-        headers: { "Content-type": "application/json;charset=UTF-8" },
+        headers: {"Content-type": "application/json;charset=UTF-8"},
       })
-        .then(() => {
-          window.location.href = "/bfr";
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      .then(() => {
+        window.location.href = "/bfr";
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     }
   };
 
   const handleOnClick = () => {
-    if (!handleNoInput()) return false;
-    if (!handleInvalidInput()) return false;
+    if (!handleNoInput()) {
+      return false;
+    }
+    if (!handleInvalidInput()) {
+      return false;
+    }
     return handleConfirmInput();
   };
 
@@ -134,151 +148,137 @@ function Survey() {
     window.location.href = "/";
   } else {
     return (
-      <section className="section section-shaped section-lg">
-        <div className="shape shape-style-1 shape-default">
-          <img
-            alt="..."
-            src={require("assets/img/theme/background.png")}
-            style={{
-              width: "100%",
-              height: "1200px",
-              maxWidth: "2000px",
-              margin: "0 auto",
-            }}
-          />
-        </div>
-        <img
-          alt="logo"
-          src={require("assets/img/brand/bank_chall_logo.png")}
-          style={{ width: "150px", margin: "0 auto", display: "flex" }}
-        />
-        <Typography variant="h3" align="center">
-          Bank Challenger
-        </Typography>
-        <br />
-        <div id="survey" margin="0 auto" display="flex">
-          <Card
-            sx={{
-              width: 2 / 5,
-              margin: "0 auto",
-              minWidth: 300,
-              border: "1px solid black",
-              boxShadow: "0 1px 3px 0px rgba(0, 0, 0, 0.5)",
-              borderRadius: 23,
-            }}
-          >
-            <br />
-            <Box
-              component="form"
-              noValidate
-              sx={{
-                width: "75%",
-                margin: "0 auto",
-                minWidth: 300,
-              }}
-            >
-              <Typography variant="h3" align="center">
-                설문조사
-              </Typography>
-              <FormGroup controlId="isMarried" sx={{ margin: 2 }}>
-                <FormLabel>결혼 여부</FormLabel>
-                <RadioGroup
-                  value={isMarried}
-                  defaultValue="미혼"
-                  onChange={handleChangeIsMarried}
-                  row
+        <section className="section section-shaped section-lg">
+          <div className="shape shape-style-1 shape-default">
+            <img
+                alt="..."
+                src={require("assets/img/theme/background.png")}
+                style={{
+                  width: "100%",
+                  height: "1200px",
+                  maxWidth: "2000px"
+                }}
+            />
+          </div>
+          <h1 style={{ textAlign: "center", margin: "20px 20px" }}>
+            <Typography class="nav-link" style={{ color: "white", fontSize: "40px" }}>
+              설문조사
+            </Typography>
+          </h1>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid style={{width: "30%", margin: "auto"}}>
+              <Item elevation={8}>
+                <Box
+                    component="form"
+                    noValidate
+                    sx={{
+                      width: "75%",
+                      margin: "0 auto",
+                      minWidth: 300,
+                    }}
                 >
-                  <FormControlLabel
-                    value="미혼"
-                    control={<Radio />}
-                    label="미혼"
-                  />
-                  <FormControlLabel
-                    value="기혼"
-                    control={<Radio />}
-                    label="기혼"
-                  />
-                </RadioGroup>
-              </FormGroup>
-              <FormGroup controlId="monthlyIncome" sx={{ margin: 2 }}>
-                <FormLabel>월 소득(단위: 만 원)</FormLabel>
-                <Input
-                  value={monthlyIncome}
-                  onChange={handleChangeMonthlyIncome}
-                  type="number"
-                  placeholder="월 소득 입력"
-                />
-              </FormGroup>
-              <FormGroup controlId="spendingRatio" sx={{ margin: 2 }}>
-                <FormLabel>월 소득에서 소비의 비율</FormLabel>
-                <Slider
-                  value={spendingRatio}
-                  onChange={handleChangeSpendingRatio}
-                  min={0}
-                  max={100}
-                  step={1}
-                />
-                <Typography variant="body" align="right">
-                  지출:{" "}
-                  {numberWithCommas(
-                    Math.round((monthlyIncome * spendingRatio) / 100)
-                  )}
-                  만 원 ({spendingRatio}%)
-                </Typography>
-              </FormGroup>
-              <FormGroup controlId="occupation" sx={{ margin: 2 }}>
-                <FormLabel>직종</FormLabel>
-                <Select value={occupation} onChange={handleChangeOccupation}>
-                  <MenuItem value={"선택"}>선택</MenuItem>
-                  <MenuItem value={"무직"}>무직</MenuItem>
-                  <MenuItem value={"학생"}>학생</MenuItem>
-                  <MenuItem value={"회사원"}>회사원</MenuItem>
-                  <MenuItem value={"자영업자"}>자영업자</MenuItem>
-                  <MenuItem value={"전문직"}>전문직</MenuItem>
-                  <MenuItem value={"프리랜서"}>프리랜서</MenuItem>
-                  <MenuItem value={"공무원"}>공무원</MenuItem>
-                  <MenuItem value={"엔지니어"}>엔지니어</MenuItem>
-                  <MenuItem value={"서비스직"}>서비스직</MenuItem>
-                </Select>
-              </FormGroup>
-              <FormGroup controlId="savings" sx={{ margin: 2 }}>
-                <FormLabel>예적금 현황(단위: 만 원)</FormLabel>
-                <Input
-                  value={savings}
-                  onChange={handleChangeSavings}
-                  type="number"
-                  placeholder="예적금 현황 입력"
-                />
-              </FormGroup>
-              <div></div>
-              <FormGroup controlId="loan" sx={{ margin: 2 }}>
-                <FormLabel>대출금 현황(단위: 만 원)</FormLabel>
-                <Input
-                  value={loan}
-                  onChange={handleChangeLoan}
-                  type="number"
-                  placeholder="대출금 현황 입력"
-                />
-              </FormGroup>
-              <FormGroup controlId="button" sx={{ margin: 2 }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleOnClick}
-                  noValidate
-                  sx={{
-                    mt: 1,
-                    width: "100%",
-                  }}
-                >
-                  설문조사 제출
-                </Button>
-              </FormGroup>
-              <br />
-            </Box>
-          </Card>
-        </div>
-      </section>
+                  <FormGroup className={"MuiFormControl-horizontal"} controlId="isMarried" sx={{margin: 2}}>
+                    <FormLabel>결혼 여부</FormLabel>
+                    <RadioGroup
+                        value={isMarried}
+                        defaultValue="미혼"
+                        style={{display: "center"}}
+                        onChange={handleChangeIsMarried}
+                        row
+                    >
+                      <FormControlLabel
+                          value="미혼"
+                          control={<Radio/>}
+                          label="미혼"
+                      />
+                      <FormControlLabel
+                          value="기혼"
+                          control={<Radio/>}
+                          label="기혼"
+                      />
+                    </RadioGroup>
+                  </FormGroup>
+                  <FormGroup controlId="monthlyIncome" sx={{margin: 2}}>
+                    <FormLabel>월 소득(단위: 만 원)</FormLabel>
+                    <Input
+                        value={monthlyIncome}
+                        onChange={handleChangeMonthlyIncome}
+                        type="number"
+                        placeholder="월 소득 입력"
+                    />
+                  </FormGroup>
+                  <FormGroup controlId="spendingRatio" sx={{margin: 2}}>
+                    <FormLabel>월 소득에서 소비의 비율</FormLabel>
+                    <Slider
+                        value={spendingRatio}
+                        onChange={handleChangeSpendingRatio}
+                        min={0}
+                        max={100}
+                        step={1}
+                    />
+                    <Typography variant="body" align="right">
+                      지출:{" "}
+                      {numberWithCommas(
+                          Math.round((monthlyIncome * spendingRatio) / 100)
+                      )}
+                      만 원 ({spendingRatio}%)
+                    </Typography>
+                  </FormGroup>
+                  <FormGroup controlId="occupation" sx={{margin: 2}}>
+                    <FormLabel>직종</FormLabel>
+                    <Select value={occupation} onChange={handleChangeOccupation}>
+                      <MenuItem value={"선택"}>선택</MenuItem>
+                      <MenuItem value={"무직"}>무직</MenuItem>
+                      <MenuItem value={"학생"}>학생</MenuItem>
+                      <MenuItem value={"회사원"}>회사원</MenuItem>
+                      <MenuItem value={"자영업자"}>자영업자</MenuItem>
+                      <MenuItem value={"전문직"}>전문직</MenuItem>
+                      <MenuItem value={"프리랜서"}>프리랜서</MenuItem>
+                      <MenuItem value={"공무원"}>공무원</MenuItem>
+                      <MenuItem value={"엔지니어"}>엔지니어</MenuItem>
+                      <MenuItem value={"서비스직"}>서비스직</MenuItem>
+                    </Select>
+                  </FormGroup>
+                  <FormGroup controlId="savings" sx={{margin: 2}}>
+                    <FormLabel>예적금 현황(단위: 만 원)</FormLabel>
+                    <Input
+                        value={savings}
+                        onChange={handleChangeSavings}
+                        type="number"
+                        placeholder="예적금 현황 입력"
+                    />
+                  </FormGroup>
+                  <div></div>
+                  <FormGroup controlId="loan" sx={{margin: 2}}>
+                    <FormLabel>대출금 현황(단위: 만 원)</FormLabel>
+                    <Input
+                        value={loan}
+                        onChange={handleChangeLoan}
+                        type="number"
+                        placeholder="대출금 현황 입력"
+                    />
+                  </FormGroup>
+                  <FormGroup controlId="button" sx={{margin: 2}}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleOnClick}
+                        noValidate
+                        sx={{
+                          mt: 1,
+                          width: "100%",
+                        }}
+                    >
+                      설문조사 제출
+                    </Button>
+                  </FormGroup>
+                  <br/>
+                </Box>
+              </Item>
+            </Grid>
+          </Box>
+        </section>
     );
   }
 }
